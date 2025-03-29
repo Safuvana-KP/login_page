f32dd2f571563a0bcda3c095d65859b9583026db
Safuvana KP <safuvanasinan@gmail.com>
2025-03-30 00:08:06 +0530

    login page demo

---
+++ lib/login_screen.dart
@@ -0,0 +1,95 @@
+import 'package:flutter/material.dart';
+import 'Home_page.dart';
+
+
+class LoginScreen extends StatelessWidget {
+  final _usernameController=TextEditingController();
+  final _passwordController=TextEditingController();
+  bool _isDataMatched=true;
+  final _formKey=GlobalKey<FormState>();
+
+  RegExp user_valid=RegExp(r"(@!$%bcefghjklopqrstuvwxyzBCEFGHJKLOPQRSTUVWXYZ[0-9])");
+  RegExp pass_valid=RegExp(r"(!$%bcefghjklopqrstuvwxyzBCEFGHJKLOPQRSTUVWXYZ0456789)");
+
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: SafeArea(
+        child:Padding(
+          padding: const EdgeInsets.all(16.0),
+          child:  Form(
+            key: _formKey,
+            child: Column(
+              crossAxisAlignment: CrossAxisAlignment.end,
+            children: [
+              SizedBox(height: 40,),
+              TextFormField(
+                controller: _usernameController,
+                decoration: InputDecoration(
+                  border: OutlineInputBorder(),
+                  labelText: "Username",
+                ),
+                keyboardType: TextInputType.emailAddress,
+                onFieldSubmitted: (value){
+                },
+                validator: (value) {
+                  if (user_valid.hasMatch(value!)){
+                  return 'error';
+                }else{
+                    return null;
+                  }
+    }
+              ),
+              SizedBox(
+                height: 20,
+              ),
+              TextFormField(
+                controller: _passwordController,
+                decoration: InputDecoration(
+                  border: OutlineInputBorder(),
+                  labelText: "password",
+                ),
+                keyboardType: TextInputType.emailAddress,
+                obscureText: true,
+                  validator: (value) {
+                    if (pass_valid.hasMatch(value!)){
+                      return 'error';
+                    }else{
+                      return null;
+                    }
+                  }
+              ),
+              SizedBox(
+                height: 20,
+              ),
+
+              ElevatedButton.icon(
+                onPressed: (){
+                if( _formKey.currentState!.validate()) {
+                  Navigator.of(context).pushReplacement(MaterialPageRoute(builder:(context)=>HomePage()));
+                }else{
+                  showDialog(context: context, builder: (ctx){
+                    return AlertDialog(title: Text("Error"),
+                      content: Text("username or password incorrect"),
+                      actions: [
+                        TextButton(onPressed:(){
+                          Navigator.of(ctx).pop();
+                        },
+                          child: Text("close"),),
+                      ],);
+                  });;
+                }   
+                },
+                icon: Icon(Icons.check),
+                label: Text("Login"),),
+            ],
+                    ),
+          ),),
+      )
+    );
+  }
+
+  }
+
+
---
+++ lib/main.dart
@@ -0,0 +1,20 @@
+import 'package:flutter/material.dart';
+import 'login_screen.dart';
+import 'package:project1/login_screen.dart';
+
+void main() {
+  runApp(MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  // This widget is the root of your application.
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      debugShowCheckedModeBanner: false,
+      title: 'Login screen',
+      theme: ThemeData(brightness: Brightness.dark),
+      home: LoginScreen(),
+    );
+  }
+}
