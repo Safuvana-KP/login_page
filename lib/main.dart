eb18018f6fd36e6e4582be87f8d25ea855cd14e9
Safuvana KP <safuvanasinan@gmail.com>
2025-04-25 19:26:05 +0530

    login page demo

--- lib/main.dart
+++ lib/main.dart
@@ -17,11 +17,16 @@ class MyApp extends StatelessWidget {
   // This widget is the root of your application.
   @override
   Widget build(BuildContext context) {
-    return MaterialApp(
+    return  MultiProvider(
+        providers: [
+      ChangeNotifierProvider(create: (_)=>CartProvider()),
+     ],
+     child: MaterialApp(
       debugShowCheckedModeBanner: false,
       title: 'Login screen',
       theme: ThemeData(brightness: Brightness.light),
       home: LoginScreen(),
+     ),
     );
   }
 }
